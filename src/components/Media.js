7d309510bd970c49711bb44db8179a446fd4dcec
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-06-13 16:26:59 -0400

    added router and nav bar

---
+++ src/components/Media.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Media = () => {
+  return (
+    <div className="Media">
+      <h1 className="Home-Header">Media Page</h1>
+      <div></div>
+      <p></p> <h2>Media Goes Here!</h2>
+    </div>
+  );
+};
