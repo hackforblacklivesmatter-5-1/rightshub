166788069f60f06a7e4f381970c00e9092df8dc0
Scott Kutler <scottkutler@gmail.com>
2020-06-13 13:31:19 -0400

    Revert "Scott table"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Table } from "./components/Table";
-import GlobalFonts from "./fonts/fonts";
+import logo from "./logoblm.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <GlobalFonts />
       <header className="App-header">
-        <Table />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to begin!
+        </p>
       </header>
     </div>
   );
