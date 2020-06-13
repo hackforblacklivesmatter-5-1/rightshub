ebf66584266876168d65af986cb1619bc30e9b83
thenatural86 <philip.smalls86@gmail.com>
2020-06-13 15:41:43 -0400

    render table data

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,6 +4,23 @@ import data from "../data/data.json"
 export const Table = () => {
   console.log(data)
 
+  const renderData = () => {
+    return data.map((record, id) => {
+      return (
+        <tr key={id}>
+          <td>{record["Victim's race"]}</td>
+          <td>{record["Victim's name"]}</td>
+          <td>{record["Cause of death"]}</td>
+          <td>{record["Criminal Charges?"]}</td>
+          <td>{record["Fleeing (Source: WaPo)"]}</td>
+          <td>{record["Alleged Weapon (Source: WaPo)"]}</td>
+          <td>{record["Alleged Threat Level (Source: WaPo):"]}</td>
+          <td>{record["Body Camera (Source: WaPo)"]}</td>
+        </tr>
+      )
+    })
+  }
+
   return (
     <div className="table-component">
       <h1 className="table-title">Shooting Incidents</h1>
@@ -12,12 +29,18 @@ export const Table = () => {
         <table className="table-content">
           <thead>
             <tr>
-              <th>Race</th>
-              <th>Name</th>
+              <th>Victim's Race</th>
+              <th>Victim's Name</th>
+              <th>Cause of Death</th>
+              <th>Criminal Charges</th>
+              <th>Fleeing</th>
+              <th>Alleged Weapon</th>
+              <th>Alleged Threat Level</th>
+              <th>Body Camera</th>
             </tr>
           </thead>
           <tbody>
-            {/* {renderData()} */}
+            {renderData()}
             {/* {sortByRace()} */}
             {/* {sortByName()} */}
           </tbody>
@@ -26,3 +49,26 @@ export const Table = () => {
     </div>
   )
 }
+
+// const sortByRace = () => {
+//   return data
+//     .sort((a, b) => (a["Victim's race"] > b["Victim's race"] ? 1 : -1))
+//     .map((record, id) => {
+//       return (
+//         <tr key={id}>
+//           <td>{record["Victim's race"]}</td>
+//         </tr>
+//       )
+//     })
+// }
+// const sortByName = () => {
+//   return data
+//     .sort((a, b) => (a["Victim's name"] > b["Victim's name"] ? 1 : -1))
+//     .map((record, id) => {
+//       return (
+//         <tr key={id}>
+//           <td>{record["Victim's name"]}</td>
+//         </tr>
+//       )
+//     })
+// }
