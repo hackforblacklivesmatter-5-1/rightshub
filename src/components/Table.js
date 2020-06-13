81e44563872f19a550e3f673885dff099eb003ad
thenatural86 <philip.smalls86@gmail.com>
2020-06-13 14:09:30 -0400

    data

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,79 +1,27 @@
-import React, { useState, useEffect } from "react"
-import axios from "axios"
+import React from "react"
+import data from "../data/data.json"
 
 export const Table = () => {
-  const [records, setRecords] = useState([])
-
-  useEffect(() => {
-    axios
-      // shooting incident data historic
-      // .get("https://data.cityofnewyork.us/resource/833y-fsy8.json")
-      // shooting incident data YTD
-      // .get("https://data.cityofnewyork.us/resource/5ucz-vwe8.json")
-      // Arrest data historic
-      .get("https://data.cityofnewyork.us/resource/8h9b-rp9u.json")
-      // Arrest data YTD
-      // .get("https://data.cityofnewyork.us/resource/uip8-fykc.json")
-      .then((res) => {
-        // console.log(res.data)
-        setRecords(res.data)
-      })
-      .catch((err) => {
-        // console.log(err)
-      })
-  }, [])
-
-  const renderTable = () => {
-    console.log(records)
-    return records.map((record, id) => {
-      return (
-        <tr key={id}>
-          <td>{record.arrest_boro}</td>
-          <td>{record.arrest_date}</td>
-          <td>{record.arrest_precinct}</td>
-          <td>{record.age_group}</td>
-        </tr>
-      )
-    })
-  }
+  console.log(data)
 
   return (
     <div>
-      <h1>Shooting Incidents</h1>
+      <h1>Killed by Police</h1>
       <table className="table">
-        <thead>
-          <tr>
-            <th>Boro</th>
-            <th>Date</th>
-            {/* <th>Age Group</th>
-            <th>Race</th>
-            <th>Gender</th> */}
-            <th>Precinct</th>
-            <th>Killing</th>
-            <th>Victim Age Group</th>
-            <th>Victim Race</th>
-            <th>Victim Gender</th>
-          </tr>
-        </thead>
-        <tbody>{renderTable()}</tbody>
+        <thead></thead>
       </table>
       <div className="table-container">
         <table className="table-content">
           <thead>
             <tr>
-              <th>Borough</th>
-              <th>Date</th>
-              <th>Age Group</th>
               <th>Race</th>
-              <th>Gender</th>
-              <th>Precinct</th>
-              <th>Killing</th>
-              <th>Victim Age Group</th>
-              <th>Victim Race</th>
-              <th>Victim Gender</th>
             </tr>
           </thead>
-          <tbody>{renderTable()}</tbody>
+          <tbody>
+            {/* {renderData()} */}
+            {/* {sortByRace()} */}
+            {/* {sortByName()} */}
+          </tbody>
         </table>
       </div>
     </div>
