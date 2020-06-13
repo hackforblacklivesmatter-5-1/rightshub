20e13ded78c7ada69bebb1e870900fd55a3a563d
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-06-13 17:17:13 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { Table } from "./components/Table";
 import GlobalFonts from "./fonts/fonts";
 import { Route } from "react-router-dom";
@@ -10,16 +9,12 @@ import { Home } from "./components/Home";
 import Navbar from "./components/Navbar";
 import { Media } from "./components/Media";
 import { Resources } from "./components/Resources";
-=======
-import logo from "./logoblm.png";
 import "./App.css";
->>>>>>> 53b783a4f5b5255ea0af7b5ff80527ecabd90e5c
 
 function App() {
   console.log(data[0]["Street Address of Incident"]);
   return (
     <div className="App">
-<<<<<<< HEAD
       <GlobalFonts />
       <Navbar></Navbar>
       <Route path="/" exact component={Home} />
@@ -27,14 +22,6 @@ function App() {
       <Route path="/Markers" exact component={Markers} />
       <Route path="/Media" exact component={Media} />
       <Route path="/Resources" exact component={Resources} />
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to begin!
-        </p>
-      </header>
->>>>>>> 53b783a4f5b5255ea0af7b5ff80527ecabd90e5c
     </div>
   );
 }
