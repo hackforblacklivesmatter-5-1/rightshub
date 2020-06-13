7763e4350c57930ed30898ee39225e3a4f96a797
Nicole <nicoledbanta@gmail.com>
2020-06-13 09:55:40 -0700

    Import police data

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import logo from "./logoblm.png";
 import "./index.css";
 import Markers from './components/Markers';
+import data from './data/data.json';
 
 function App() {
+  console.log(data[0]["Street Address of Incident"])
   return (
     <div className="App">
       <header className="App-header">
