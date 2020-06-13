73ccce83b3d18b56706eae24a557de6e2ba83cce
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-06-13 17:09:01 -0400

    merge from develop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+<<<<<<< HEAD
 import { Table } from "./components/Table";
 import GlobalFonts from "./fonts/fonts";
 import { Route } from "react-router-dom";
@@ -9,11 +10,16 @@ import { Home } from "./components/Home";
 import Navbar from "./components/Navbar";
 import { Media } from "./components/Media";
 import { Resources } from "./components/Resources";
+=======
+import logo from "./logoblm.png";
+import "./App.css";
+>>>>>>> 53b783a4f5b5255ea0af7b5ff80527ecabd90e5c
 
 function App() {
   console.log(data[0]["Street Address of Incident"]);
   return (
     <div className="App">
+<<<<<<< HEAD
       <GlobalFonts />
       <Navbar></Navbar>
       <Route path="/" exact component={Home} />
@@ -21,6 +27,14 @@ function App() {
       <Route path="/Markers" exact component={Markers} />
       <Route path="/Media" exact component={Media} />
       <Route path="/Resources" exact component={Resources} />
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to begin!
+        </p>
+      </header>
+>>>>>>> 53b783a4f5b5255ea0af7b5ff80527ecabd90e5c
     </div>
   );
 }
