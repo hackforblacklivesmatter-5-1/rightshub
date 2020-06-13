1d31df964a8d1b6ff4434b3b28c097f84e769b81
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-06-13 14:07:42 -0400

    installed router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from "react";
 import { Table } from "./components/Table";
 import GlobalFonts from "./fonts/fonts";
-
-import logo from "./logoblm.png";
+import { Route } from "react-router-dom";
 import "./index.css";
-import Markers from './components/Markers';
-import data from './data/data.json';
+import Markers from "./components/Markers";
+import data from "./data/data.json";
+import { Home } from "./components/Home";
 
 function App() {
   console.log(data[0]["Street Address of Incident"]);
   return (
     <div className="App">
       <GlobalFonts />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to begin!</p>
-      </header>
-      <Markers />
+      <Route path="/" exact component={Home} />
+      <Route path="/Table" exact component={Table} />
+      <Route path="/Markers" exact component={Markers} />
     </div>
   );
 }
