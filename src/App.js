d4146d4de3f0e851b1f9e250a153b6bd08eee49e
Nicole <nicoledbanta@gmail.com>
2020-06-13 08:50:06 -0700

    Update markers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import logo from "./logoblm.png";
 import "./App.css";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import useSwr from "swr";
+const fetcher = (...args) => fetch(...args).then(response => response.json());
 
 function App() {
+  const url =
+    "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
+  const { data, error } = useSwr(url, { fetcher });
+  const crimes = data && !error ? data.slice(0, 100) : [];
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +19,20 @@ function App() {
           Edit <code>src/App.js</code> and save to begin!
         </p>
       </header>
+
+      <Map center={[52.6376, -1.135171]} zoom={12}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+
+        {crimes.map(crime => (
+          <Marker
+            key={crime.id}
+            position={[crime.location.latitude, crime.location.longitude]}
+          />
+        ))}
+      </Map>
     </div>
   );
 }
