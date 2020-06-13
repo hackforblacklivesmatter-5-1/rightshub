b7effee86cb285726fdf91a4de05a374bde98c4e
Nicole <nicoledbanta@gmail.com>
2020-06-13 12:38:57 -0700

    Assign markers across North America

--- src/components/Markers.js
+++ src/components/Markers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import useSwr from "swr";
-import { Map, Marker, TileLayer } from "react-leaflet";
+import { Map, Marker, TileLayer, Popup } from "react-leaflet";
 import police from '../data/data.json';
 
 const fetcher = (...args) => fetch(...args).then(response => response.json());
@@ -20,18 +20,28 @@ const Markers = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      {crimes.map(crime => (
-        <Marker
-          key={crime.id}
-          position={[crime.location.latitude, crime.location.longitude]}
-        />
-      ))}
       {police.map(popo => (
         <Marker
           key={popo["WaPo ID (If included in WaPo database)"]}
           position={[popo['Latitude'], popo['Longitude']]}
-        />
+        >
+        </Marker>
       ))}
+      <Marker position={[40.7128, -74.0060]}>
+        <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+        <Popup
+          position={[40.7128, -74.0060]}
+          onClose={() => {
+            alert('closed popup')
+          }}
+        >
+          <div>
+            {/* <h2>{activePark.properties.NAME}</h2>
+          <p>{activePark.properties.DESCRIPTIO}</p> */}
+            <p>I am a popup</p>
+          </div>
+        </Popup>
+      </Marker>
     </Map>
   )
 }
