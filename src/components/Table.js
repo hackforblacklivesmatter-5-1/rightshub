95b7c9ed2fc83419c729881afdff48ffb536d5f7
Kevin Wang <kcwang06@gmail.com>
2020-06-13 21:44:02 -0400

    add material ui styling to sliders

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import data from '../data/data-ny.json'
-import { Statistics } from './Statistics'
+import Slider from '@material-ui/core/Slider'
+import { makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
 
 export const Table = () => {
 	console.log(data)
@@ -12,7 +14,8 @@ export const Table = () => {
 					<td>{record["Victim's race"]}</td>
 					<td>{record["Victim's name"]}</td>
 					<td>{record['Cause of death']}</td>
-					<td>{record['Criminal Charges?']}</td>
+					<td>{record["Victim's age"]}</td>
+					<td>{record['Date of Incident (month/day/year)']}</td>
 					<td>{record['Fleeing (Source: WaPo)']}</td>
 					<td>{record['Alleged Weapon (Source: WaPo)']}</td>
 					<td>{record['Alleged Threat Level (Source: WaPo):']}</td>
@@ -21,12 +24,71 @@ export const Table = () => {
 			)
 		})
 	}
+	// slider for year
+	const useStyles = makeStyles({
+		root: {
+			width: 300,
+		},
+	})
+	function valuetext(value) {
+		return `${value}°C`
+	}
+
+	// filter by race
+	const filterByRace = () => {
+		const blackVictims = data.filter(
+			(victim) => victim["Victim's race"] === 'Black'
+		)
+		console.log('blackVictims: ', blackVictims)
+	}
+
+	const sortByDate = () => {
+		let sorted = data.sort((a, b) =>
+			new Date(a['Date of Incident (month/day/year)']) <
+			new Date(b['Date of Incident (month/day/year)'])
+				? 1
+				: -1
+		)
+		console.log(sorted)
+	}
+
+	const sortByAge = () => {
+		let sorted = data.sort((a, b) =>
+			parseInt(a["Victim's age"]) < parseInt(b["Victim's age"]) ? 1 : -1
+		)
+		console.log(sorted)
+	}
+
+	const sortByGun = () => {
+		let sorted = data.sort((a, b) =>
+			a['Cause of death'] > b['Cause of death'] ? 1 : -1
+		)
+		console.log(sorted)
+	}
 
 	return (
 		<div className='table-component'>
-			<h1 className='table-title'>Shooting Incidents</h1>
+			<h1 className='table-title'>Killings by Police</h1>
 			<div className='yellow-line'></div>
-			<Statistics />
+			<div className='toggle'>
+				<label onClick={() => filterByRace()}>Black Victims</label>
+				<label onClick={() => sortByDate()}>Sorted Race</label>
+				<label onClick={() => sortByAge()}>Sorted Age</label>
+				<label onClick={() => sortByGun()}>Sorted Gun</label>
+				<Typography id='discrete-slider' gutterBottom>
+					Year
+				</Typography>
+				<Slider
+					defaultValue={0}
+					getAriaValueText={valuetext}
+					aria-labelledby='discrete-slider'
+					valueLabelDisplay='auto'
+					step={6}
+					marks
+					min={2013}
+					max={2019}
+				/>
+			</div>
 			<div className='table-container'>
 				<table className='table-content'>
 					<thead>
@@ -34,7 +96,8 @@ export const Table = () => {
 							<th>Victim's Race</th>
 							<th>Victim's Name</th>
 							<th>Cause of Death</th>
-							<th>Criminal Charges</th>
+							<th>Victim's age</th>
+							<th>Date of Incident (month/day/year)</th>
 							<th>Fleeing</th>
 							<th>Alleged Weapon</th>
 							<th>Alleged Threat Level</th>
@@ -52,25 +115,34 @@ export const Table = () => {
 	)
 }
 
-// const sortByRace = () => {
-//   return data
-//     .sort((a, b) => (a["Victim's race"] > b["Victim's race"] ? 1 : -1))
-//     .map((record, id) => {
-//       return (
-//         <tr key={id}>
-//           <td>{record["Victim's race"]}</td>
-//         </tr>
-//       )
-//     })
-// }
-const sortByName = () => {
-	return data
-		.sort((a, b) => (a["Victim's name"] > b["Victim's name"] ? 1 : -1))
-		.map((record, id) => {
-			return (
-				<tr key={id}>
-					<td>{record["Victim's name"]}</td>
-				</tr>
-			)
-		})
-}
+// .map((record, id) => {
+//   return (
+//     // <tr key={id}>
+//     //   <td>{record["Date of Incident (month/day/year)"]}</td>
+//     // </tr>
+//   )
+// })
+
+// Filter victims by being from New York City - can be anywhere
+// const filterNewYork = () => {
+//   const newYorkVictims = data.filter(victim => victim.City === 'New York')
+//   console.log('New York victims: ', newYorkVictims)
+//   return newYorkVictims
+
+// };
+
+// const filterByGunshot = () => {
+//   const gunshotVictims = data.filter(victim => victim["Cause of death"] === "Gunshot")
+//   console.log('gunshotVictims: ', gunshotVictims)
+//   // data.forEach(victim => console.log(victim["Cause of death"]))
+// };
+
+// const filterByNoCriminalCharges = () => {
+//   const noCriminalCharges = data.filter(victim => victim["Criminal Charges?"] === "No known charges")
+//   console.log('noCriminalCharges: ', noCriminalCharges)
+// };
+
+// const filterByAllegedWeapons = () => {
+//   const allegedWeapons = data.map(victim => victim["Alleged Weapon (Source: WaPo)"])
+//   console.log('allegedWeapons: ', allegedWeapons);
+// };
