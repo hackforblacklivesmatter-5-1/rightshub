ca483f7c2a73980674edd96ab4213d9301ea96db
thenatural86 <philip.smalls86@gmail.com>
2020-06-14 12:40:05 -0400

    rights card

---
+++ src/components/Gallery/RightsCard.js
@@ -0,0 +1,18 @@
+import React from "react"
+const RightsCard = (props) => {
+  return (
+    <div className="card text-center">
+      <div className="overflow">
+        {/* <img src={props.imgsrc} alt="ACLU" className="card-img-top" /> */}
+      </div>
+      <div className="card-box text-dark">
+        <h4 className="card-title">{props.title}</h4>
+        <p className="card-text text-secondary">{props.desc}</p>
+        <a href={props.href} className="btn btn-outline-success">
+          Learn More
+        </a>
+      </div>
+    </div>
+  )
+}
+export default RightsCard
