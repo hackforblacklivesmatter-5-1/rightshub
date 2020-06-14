3f7d47b4510427c8226c98f8dbd3f7006821aca8
thenatural86 <philip.smalls86@gmail.com>
2020-06-13 21:10:27 -0400

    sort/filter functions

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import data from "../data/data-ny.json"
 
 export const Table = () => {
-  console.log(data)
+  const [race, setRace] = useState(false)
+  const [date, setDate] = useState(false)
+  const [age, setAge] = useState(false)
+  const [gun, setGun] = useState(false)
 
   const renderData = () => {
     return data.map((record, id) => {
@@ -10,7 +13,13 @@ export const Table = () => {
         <tr key={id}>
           <td>{record["Victim's race"]}</td>
           <td>{record["Victim's name"]}</td>
-          <td>{record["Cause of death"]}</td>
+          <td>
+            {
+              record[
+                "A brief description of the circumstances surrounding the death"
+              ]
+            }
+          </td>
           <td>{record["Victim's age"]}</td>
           <td>{record["Date of Incident (month/day/year)"]}</td>
           <td>{record["Fleeing (Source: WaPo)"]}</td>
@@ -26,31 +35,127 @@ export const Table = () => {
     const blackVictims = data.filter(
       (victim) => victim["Victim's race"] === "Black"
     )
-    console.log("blackVictims: ", blackVictims)
+    return blackVictims.map((record, id) => {
+      return (
+        <tr key={id}>
+          <td>{record["Victim's race"]}</td>
+          <td>{record["Victim's name"]}</td>
+          <td>
+            {
+              record[
+                "A brief description of the circumstances surrounding the death"
+              ]
+            }
+          </td>
+          <td>{record["Victim's age"]}</td>
+          <td>{record["Date of Incident (month/day/year)"]}</td>
+          <td>{record["Fleeing (Source: WaPo)"]}</td>
+          <td>{record["Alleged Weapon (Source: WaPo)"]}</td>
+          <td>{record["Alleged Threat Level (Source: WaPo):"]}</td>
+          <td>{record["Body Camera (Source: WaPo)"]}</td>
+        </tr>
+      )
+    })
   }
 
   const sortByDate = () => {
     let sorted = data.sort((a, b) =>
-      new Date(a["Date of Incident (month/day/year)"]) <
+      new Date(a["Date of Incident (month/day/year)"]) >
       new Date(b["Date of Incident (month/day/year)"])
         ? 1
         : -1
     )
-    console.log(sorted)
+    return sorted.map((record, id) => {
+      return (
+        <tr key={id}>
+          <td>{record["Victim's race"]}</td>
+          <td>{record["Victim's name"]}</td>
+          <td>
+            {
+              record[
+                "A brief description of the circumstances surrounding the death"
+              ]
+            }
+          </td>
+          <td>{record["Victim's age"]}</td>
+          <td>{record["Date of Incident (month/day/year)"]}</td>
+          <td>{record["Fleeing (Source: WaPo)"]}</td>
+          <td>{record["Alleged Weapon (Source: WaPo)"]}</td>
+          <td>{record["Alleged Threat Level (Source: WaPo):"]}</td>
+          <td>{record["Body Camera (Source: WaPo)"]}</td>
+        </tr>
+      )
+    })
   }
 
   const sortByAge = () => {
     let sorted = data.sort((a, b) =>
-      parseInt(a["Victim's age"]) < parseInt(b["Victim's age"]) ? 1 : -1
+      parseInt(a["Victim's age"]) > parseInt(b["Victim's age"]) ? 1 : -1
     )
-    console.log(sorted)
+    return sorted.map((record, id) => {
+      return (
+        <tr key={id}>
+          <td>{record["Victim's race"]}</td>
+          <td>{record["Victim's name"]}</td>
+          <td>
+            {
+              record[
+                "A brief description of the circumstances surrounding the death"
+              ]
+            }
+          </td>
+          <td>{record["Victim's age"]}</td>
+          <td>{record["Date of Incident (month/day/year)"]}</td>
+          <td>{record["Fleeing (Source: WaPo)"]}</td>
+          <td>{record["Alleged Weapon (Source: WaPo)"]}</td>
+          <td>{record["Alleged Threat Level (Source: WaPo):"]}</td>
+          <td>{record["Body Camera (Source: WaPo)"]}</td>
+        </tr>
+      )
+    })
   }
 
   const sortByGun = () => {
     let sorted = data.sort((a, b) =>
       a["Cause of death"] > b["Cause of death"] ? 1 : -1
     )
-    console.log(sorted)
+    return sorted.map((record, id) => {
+      return (
+        <tr key={id}>
+          <td>{record["Victim's race"]}</td>
+          <td>{record["Victim's name"]}</td>
+          <td>
+            {
+              record[
+                "A brief description of the circumstances surrounding the death"
+              ]
+            }
+          </td>
+          <td>{record["Victim's age"]}</td>
+          <td>{record["Date of Incident (month/day/year)"]}</td>
+          <td>{record["Fleeing (Source: WaPo)"]}</td>
+          <td>{record["Alleged Weapon (Source: WaPo)"]}</td>
+          <td>{record["Alleged Threat Level (Source: WaPo):"]}</td>
+          <td>{record["Body Camera (Source: WaPo)"]}</td>
+        </tr>
+      )
+    })
+  }
+
+  const toggleRace = () => {
+    setRace(!race)
+  }
+
+  const toggleDate = () => {
+    setDate(!date)
+  }
+
+  const toggleAge = () => {
+    setAge(!age)
+  }
+
+  const toggleGun = () => {
+    setGun(!gun)
   }
 
   return (
@@ -60,14 +165,14 @@ export const Table = () => {
       <div className="table-container">
         <table className="table-content">
           <thead>
-            <button onClick={() => filterByRace()}>Black Victims</button>
-            <button onClick={() => sortByDate()}>Sorted Race</button>
-            <button onClick={() => sortByAge()}>Sorted Age</button>
-            <button onClick={() => sortByGun()}>Sorted Gun</button>
+            <button onClick={() => toggleRace()}>Black Lives</button>
+            <button onClick={() => toggleDate()}>Sorted Date</button>
+            <button onClick={() => toggleAge()}>Sorted Age</button>
+            {/* <button onClick={() => toggleGun()}>Sorted Gun</button> */}
             <tr>
               <th>Victim's Race</th>
               <th>Victim's Name</th>
-              <th>Cause of Death</th>
+              <th>Description</th>
               <th>Victim's age</th>
               <th>Date of Incident (month/day/year)</th>
               <th>Fleeing</th>
@@ -77,9 +182,10 @@ export const Table = () => {
             </tr>
           </thead>
           <tbody>
-            {renderData()}
-            {/* {sortByRace()} */}
-            {/* {sortByName()} */}
+            {race ? filterByRace() : renderData()}
+            {date ? sortByDate() : renderData()}
+            {age ? sortByAge() : renderData()}
+            {gun ? sortByGun() : renderData()}
           </tbody>
         </table>
       </div>
@@ -87,20 +193,11 @@ export const Table = () => {
   )
 }
 
-// .map((record, id) => {
-//   return (
-//     // <tr key={id}>
-//     //   <td>{record["Date of Incident (month/day/year)"]}</td>
-//     // </tr>
-//   )
-// })
-
 // Filter victims by being from New York City - can be anywhere
 // const filterNewYork = () => {
 //   const newYorkVictims = data.filter(victim => victim.City === 'New York')
 //   console.log('New York victims: ', newYorkVictims)
 //   return newYorkVictims
-
 // };
 
 // const filterByGunshot = () => {
