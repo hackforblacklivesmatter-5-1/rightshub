405de323600718b3727516d6e56a8cea61c3bfaf
Nicole <nicoledbanta@gmail.com>
2020-06-14 11:46:17 -0700

    Add all resources

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -5,6 +5,10 @@ import GalleryCard from "./GalleryCard";
 import aclu from "./assets/aclu-logo.jpeg";
 import jlusa from "./assets/jlusa-logo.jpeg";
 import crj from "./assets/crj-logo.png";
+import mh from './assets/blm-mh.jpeg';
+import ccrb from './assets/ccrb.png';
+import vera from './assets/vera.png';
+import blm from './assets/blm.png'
 
 class Gallery extends Component {
 
@@ -16,6 +20,10 @@ class Gallery extends Component {
         "Mass incarceration is the most significant domestic threat to the fabric of our democracy. The reason for such high incarceration rates is not serious crimes but misguided policies such as mandatory minimums, three-strikes laws and reductions in the availability of parole and other early release mechanisms.",
       crj:
         "In 2010, the mission and direction of Border Crossers shifted to training educators to disrupt and dismantle racism in their classrooms, communities, and homes. We are now the Center for Racial Justice in Education, wherein dismantling racism, we are transforming communities—across New York City and nationwide.",
+      mh: "This website contains resources to therapists and hotlines for black african americans struggling with mental health. during this uncertain and challanging time in our black community.",
+      ccrb: "If you wish to file a complaint regarding excessive force, abuse of authority, discourtesy or offensive language (FADO) against a member of the New York City Police Department (NYPD) through our web site, complete the form below. Complaints against members of police departments other than the NYPD must be filed with the Internal Affairs department.",
+      vera: "The Immigration Court Helpdesk (ICH) program educates non-detained immigrants in removal (deportation) proceedings about the court process. The goal of ICH is to help individuals make informed decisions about their legal cases and, in doing so, improve the efficiency and effectiveness of immigration court proceedings.",
+      blm: "Join the Movement to fight for Freedom, Liberation and Justice by signing up for updates, supporting our work, checking out our resources, following us on social media, or wearing our dope, official gear. See this too for more resources: https://blacklivesmatters.carrd.co/"
     };
 
     return (
@@ -46,30 +54,31 @@ class Gallery extends Component {
                 desc={description.crj}
               />
             </div>
+
             <div className="col-md-4">
               <GalleryCard
-                imgsrc={crj}
-                title="Center for Racial Justice"
-                href="https://centerracialjustice.org/"
-                desc={description.crj}
+                imgsrc={ccrb}
+                title="File a Police Misconduct"
+                href="https://www1.nyc.gov/site/ccrb/complaints/file-online.page"
+                desc={description.ccrb}
               />
             </div>
 
             <div className="col-md-4">
               <GalleryCard
-                imgsrc={crj}
-                title="Center for Racial Justice"
-                href="https://centerracialjustice.org/"
-                desc={description.crj}
+                imgsrc={vera}
+                title="Immigration Court Helpdesk"
+                href="https://www.vera.org/projects/immigration-court-helpdesk/immigration-court-helpdesk-ich-courts"
+                desc={description.vera}
               />
             </div>
 
             <div className="col-md-4">
               <GalleryCard
-                imgsrc={crj}
-                title="Center for Racial Justice"
-                href="https://centerracialjustice.org/"
-                desc={description.crj}
+                imgsrc={blm}
+                title="Black Lives Matter"
+                href="https://blacklivesmatter.com/"
+                desc={description.blm}
               />
             </div>
 
